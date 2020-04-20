76eb88cd4545b56ab14a4398ec78550244eeb0ec
JohTorm <johannes.tormanen@hotmail.fi>
2020-04-20 10:33:36 +0300

    Labroja viimeistelty

---
+++ Labra3/app/src/main/java/com/example/labra3/FootballData.java
@@ -0,0 +1,9 @@
+package com.example.labra3;
+
+public class FootballData {
+    private String competitionName;
+
+    public FootballData(String competitionName) {
+        this.competitionName = competitionName;
+    }
+}
